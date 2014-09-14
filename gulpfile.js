7a7824afaa0570cf96dc0bcee076f3bcb58a927e
Tri Nguyen <tri@tridnguyen.com>
2014-09-14 12:24:07 -0400

    gulp template task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var gutil = require('gulp-util');
+var ghandlebars = require('gulp-handlebars');
+var defineModule = require('gulp-define-module');
 var browserify = require('browserify');
+var remapify = require('remapify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var xtend = require('xtend');
@@ -26,10 +29,17 @@ gulp.task('jshint', function () {
 		.pipe(jshint.reporter(require('jshint-stylish')));
 });
 
+gulp.task('templates', function () {
+	gulp.src('app/templates/**/*.hbs')
+		.pipe(ghandlebars())
+		.pipe(defineModule('node'))
+		.pipe(gulp.dest('.tmp/templates'))
+});
+
 var watching = false;
 gulp.task('enable-watch-mode', function () { watching = true });
 
-gulp.task('scripts', ['jshint'], function () {
+gulp.task('scripts', ['jshint', 'templates'], function () {
 	var opts = {
 		entries: './app/scripts/main.js',
 		debug: (gutil.env.type === 'development')
@@ -43,6 +53,12 @@ gulp.task('scripts', ['jshint'], function () {
 	}
 	// optionally transform
 	// bundler.transform('transformer');
+
+	bundler.plugin(remapify, [{
+		src: './**/*.js',
+		expose: 'templates',
+		cwd: '.tmp/templates'
+	}]);
 
 	bundler.on('update', function (ids) {
 		gutil.log('File(s) changed: ' + gutil.colors.cyan(ids));
@@ -66,4 +82,5 @@ gulp.task('build', ['html', 'scss', 'scripts']);
 gulp.task('watch', ['enable-watch-mode', 'scripts'], function () {
 	gulp.watch('app/scss/**/*.scss', ['scss']);
 	gulp.watch('app/**/*.html', ['html']);
+	gulp.watch('app/templates/**/*.hbs', ['scripts']);
 })
