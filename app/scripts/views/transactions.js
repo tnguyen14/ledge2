f6fc76da086a4a023a37b59e95b8ce4256d3e059
Tri Nguyen <tri@tridnguyen.com>
2014-12-17 17:46:21 -0500

    display weeks
    - use subcollection to filter out weekly transactions

--- app/scripts/views/transactions.js
+++ app/scripts/views/transactions.js
@@ -4,23 +4,54 @@ var View = require('ampersand-view');
 var TransactionsCollection = require('../collections/transactions');
 var WeekView = require('./week');
 var template = require('templates/transactions');
+var SubCollection = require('ampersand-subcollection');
+var moment = require('moment-timezone');
+
+/**
+ * @description compare the date to see whether it falls within the week
+ * @param date the date ISO string to be compared
+ * @param offset the number of week offset, default to 0, i.e. the current week
+ **/
+var dateFilter = function (date, offset) {
+	offset = (offset || 0) * 7;
+	var weekStart = moment().day(1 + offset).startOf('isoWeek').toISOString();
+	var weekEnd = moment().day(7 + offset).endOf('isoWeek').toISOString();
+	return date >= weekStart && date <= weekEnd;
+};
 
 var Transactions = View.extend({
 	template: template,
 	render: function () {
 		this.renderWithTemplate();
-		var currentWeekTransactions = new TransactionsCollection([], {
-			week: 'current',
-			account: this.model.getId()
+
+		var currentWeek = new SubCollection(this.model.transactions, {
+			filter: function (model) {
+				return dateFilter(model.date);
+			},
+			comparator: function (model) {
+				return model.date;
+			}
 		});
+
+		var prevWeek = new SubCollection(this.model.transactions, {
+			filter: function (model) {
+				return dateFilter(model.date, -1);
+			},
+			comparator: function (model) {
+				return model.date;
+			}
+		});
+
 		this.currentWeekView = new WeekView({
-			collection: currentWeekTransactions
+			offset: 0,
+			collection: currentWeek
 		});
-		currentWeekTransactions.fetch({
-			success: function () {
-				this.renderSubview(this.currentWeekView);
-			}.bind(this)
+		this.prevWeekView = new WeekView({
+			offset: -1,
+			collection: prevWeek
 		});
+		this.renderSubview(this.currentWeekView);
+		this.renderSubview(this.prevWeekView);
 		return this;
 	}
 });
--- app/scripts/views/week.js
+++ app/scripts/views/week.js
@@ -2,11 +2,29 @@
 
 var View = require('ampersand-view');
 var template = require('templates/week');
+var moment = require('moment-timezone');
 
 var Week = View.extend({
+	props: {
+		offset: ['number', true, 0]
+	},
+	derived: {
+		weekStart: {
+			deps: ['offset'],
+			fn: function () {
+				return moment().day(1 + this.offset * 7).startOf('isoWeek');
+			}
+		},
+		weekEnd: {
+			deps: ['offset'],
+			fn: function () {
+				return moment().day(7 + this.offset * 7).endOf('isoWeek');
+			}
+		}
+	},
 	template: template,
 	render: function () {
-		this.renderWithTemplate(this.collection);
+		this.renderWithTemplate(this);
 		return this;
 	}
 });
