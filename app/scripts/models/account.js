36bf9ed2e3b957577ecc9eaa4b2c488e3ebee8e7
Tri Nguyen <tri@tridnguyen.com>
2015-07-24 11:11:59 -0400

    stats for each category

--- app/scripts/models/account.js
+++ app/scripts/models/account.js
@@ -3,6 +3,8 @@
 var Model = require('ampersand-model');
 var Transactions = require('../collections/transactions');
 var config = require('config');
+var statsUtil = require('../util/stats');
+var config = require('config');
 
 var Account = Model.extend({
 	idAttribute: 'name',
@@ -15,6 +17,29 @@ var Account = Model.extend({
 	},
 	collections: {
 		transactions: Transactions
+	},
+	getStats: function () {
+		var stats = {
+			averages: []
+		};
+		var numWeeks = statsUtil.totalWeeks(this.transactions);
+		var self = this;
+		config.categories.forEach(function (cat) {
+			var slug = cat.slug;
+			var items = self.transactions.models.filter(function (t) {
+				return t.category === slug;
+			});
+			var itemsTotal = items.reduce(function (total, t) {
+				return total += t.amount;
+			}, 0);
+			console.log(itemsTotal);
+			stats.averages.push({
+				label: cat.value,
+				amount: itemsTotal / numWeeks,
+				slug: slug
+			});
+		});
+		return stats;
 	}
 });
 
--- app/scripts/views/stats.js
+++ app/scripts/views/stats.js
@@ -1,30 +1,14 @@
 'use strict';
 
 var View = require('ampersand-view');
-
 var template = require('templates/stats');
-
+var config = require('config');
 var statsUtil = require('../util/stats');
 
 module.exports = View.extend({
 	template: template,
 	render: function () {
-		var stats = {
-			averages: {
-				dineout: 0
-			}
-		};
-		var dineouts, dineoutsTotal, weeks;
-		if (this.model.transactions.length !== 0) {
-			dineouts = this.model.transactions.models.filter(function (t) {
-				return t.category === 'dineout';
-			});
-			dineoutsTotal = dineouts.reduce(function (total, t) {
-				return total += t.amount;
-			}, 0);
-			weeks = statsUtil.totalWeeks(this.model.transactions);
-			stats.averages.dineout = dineoutsTotal / weeks;
-		}
+		var stats = this.model.getStats();
 		this.renderWithTemplate(stats);
 	}
 });
