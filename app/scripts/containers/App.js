a7050b22b151d6db63aa9b78367bae397538a03c
Tri Nguyen <tri@tridnguyen.com>
2015-12-19 19:52:44 -0500

    App is a container, not component

---
+++ app/scripts/containers/App.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+var connect = require('react-redux').connect;
+var Alert = require('../components/Alert');
+var NewTransaction = require('../components/NewTransaction');
+
+var actions = require('../actions');
+var getAccount = actions.getAccount;
+
+var App = React.createClass({
+	propTypes: {
+		dispatch: PropTypes.func.isRequired
+	},
+	componentDidMount: function () {
+		var dispatch = this.props.dispatch;
+		dispatch(getAccount());
+	},
+	render: function () {
+		return (
+			<div className="main">
+				<Alert/>
+				<NewTransaction/>
+			</div>
+		);
+	}
+});
+
+function mapStateToProps (state) {
+	return {
+		account: state.account
+	};
+}
+module.exports = connect(mapStateToProps)(App);
--- app/scripts/main-react.js
+++ app/scripts/main-react.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Provider = require('react-redux').Provider;
-var App = require('./components/App');
+var App = require('./containers/App');
 var configureStore = require('./store');
 
 ReactDOM.render(
