f3206197cb2ced7a7f0e6660ac98902531ae0304
Tri Nguyen <tri@tridnguyen.com>
2015-12-20 17:13:39 -0500

    support for redux-devtools-extension

--- app/scripts/store/index.js
+++ app/scripts/store/index.js
@@ -3,17 +3,21 @@
 var redux = require('redux');
 var createStore = redux.createStore;
 var applyMiddleware = redux.applyMiddleware;
+var compose = redux.compose;
 var thunkMiddleware = require('redux-thunk');
 var createLogger = require('redux-logger').default;
 var rootReducer = require('../reducers');
 
-var createStoreWithMiddleware = applyMiddleware(
-	thunkMiddleware,
-	createLogger({
-		predicate: function () {
-			return process.env.NODE_ENV === 'development';
-		}
-	})
+var createStoreWithMiddleware = compose(
+	applyMiddleware(
+		thunkMiddleware,
+		createLogger({
+			predicate: function () {
+				return process.env.NODE_ENV === 'development';
+			}
+		})
+	),
+	window.devToolsExtension ? window.devToolsExtension() : function (f) {return f;}
 )(createStore);
 
 module.exports = function (initialState) {
