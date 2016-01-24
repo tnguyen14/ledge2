895fcc2f14423f49174210f5794936a9548de238
Tri Nguyen <tri@tridnguyen.com>
2016-01-23 22:58:13 -0500

    make transaction active on click

--- app/scripts/components/Transaction.js
+++ app/scripts/components/Transaction.js
@@ -1,26 +1,44 @@
 import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 import { Badge } from 'react-bootstrap';
-import { date, money, getSource, getCategory } from '../util/helpers';
+import { date as getDate, money, getSource, getCategory } from '../util/helpers';
 
-export default function Transaction (props) {
-	return (
-		<tr className="transaction" data-transaction-id={props.data._id} data-day={date('ddd', props.data.date)}>
-			<td className="day">{date('ddd', props.data.date)}</td>
-			<td className="merchant">{props.data.merchant}</td>
-			<td className="amount" data-cat={props.data.category}><Badge>{money(props.data.amount)}</Badge></td>
-			<td className="source">{getSource(props.data.source)}</td>
-			<td className="description">{props.data.description}</td>
-			<td className="category">{getCategory(props.data.category)}</td>
-			<td className="action">
-				<i className="edit glyphicon glyphicon-pencil" onClick={props.onEditClick.bind(this, props.data._id)}></i>
-				<i className="remove glyphicon glyphicon-remove" onClick={props.onDeleteClick.bind(this, props.data._id)}></i>
-			</td>
-		</tr>
-	);
-}
+const Transaction = React.createClass({
+	propTypes: {
+		data: PropTypes.object.isRequired,
+		onEditClick: PropTypes.func.isRequired,
+		onDeleteClick: PropTypes.func.isRequired
+	},
 
-Transaction.propTypes = {
-	data: PropTypes.object.isRequired,
-	onEditClick: PropTypes.func.isRequired,
-	onDeleteClick: PropTypes.func.isRequired
-};
+	getInitialState () {
+		return {
+			active: false
+		};
+	},
+
+	handleClick () {
+		this.setState({
+			active: !this.state.active
+		});
+	},
+
+	render () {
+		const { data: { _id, date, merchant, category, amount, source, description }, onEditClick, onDeleteClick } = this.props;
+		return (
+			<tr className={classNames('transaction', {active: this.state.active})} data-transaction-id={_id} data-day={getDate('ddd', date)} onClick={this.handleClick}>
+				<td className="day">{getDate('ddd', date)}</td>
+				<td className="merchant">{merchant}</td>
+				<td className="amount" data-cat={category}><Badge>{money(amount)}</Badge></td>
+				<td className="source">{getSource(source)}</td>
+				<td className="description">{description}</td>
+				<td className="category">{getCategory(category)}</td>
+				<td className="action">
+					<i className="edit glyphicon glyphicon-pencil" onClick={onEditClick.bind(this, _id)}></i>
+					<i className="remove glyphicon glyphicon-remove" onClick={onDeleteClick.bind(this, _id)}></i>
+				</td>
+			</tr>
+		);
+	}
+});
+
+export default Transaction;
