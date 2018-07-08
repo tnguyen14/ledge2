08dedcd169c64b556c0144ea124a203d03bc3821
Tri Nguyen <tri@tridnguyen.com>
2018-07-08 15:00:37 -0400

    use react-router

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import reducer from './reducers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 render(
 	<Provider store={store}>
-		<App />
+		<Router history={history}>
+			<Route path="/" component={App} />
+		</Router>
 	</Provider>,
 	document.querySelector('.main')
 );
