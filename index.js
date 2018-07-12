b7ba1333637b2e93bdd29c07435359313471f3ac
Tri Nguyen <tri@tridnguyen.com>
2018-07-12 00:15:11 -0400

    add support for redux devtools

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import Header from './containers/header';
@@ -9,7 +10,7 @@ import reducer from './reducers';
 import { Router, Route } from 'react-router-dom';
 import history from './history';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 render(
 	<Provider store={store}>
