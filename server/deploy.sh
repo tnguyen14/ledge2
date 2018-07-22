46c7080de1d344fd7da8eed69bbd67013e59aed5
Tri Nguyen <tri@tridnguyen.com>
2018-07-22 00:25:13 -0400

    update travis deploy

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_2ef622fe4472_key -iv $encrypted_2ef622fe4472_iv -in .travis/muffin.enc -out .travis/muffin -d
+openssl aes-256-cbc -K $encrypted_7bed071027e5_key -iv $encrypted_7bed071027e5_iv -in .travis/muffin.enc -out .travis/muffin -d
 chmod 600 .travis/muffin
 echo -e "Host $DEPLOY_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval "$(ssh-agent -s)" # start the ssh agent
