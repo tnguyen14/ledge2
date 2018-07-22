b43e7dccb100c62e892f09920aa0bdfb2382c3c7
Tri Nguyen <tri@tridnguyen.com>
2018-07-22 00:32:29 -0400

    fix deploy_host env ref

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_7bed071027e5_key -iv $encrypted_7bed071027e5_iv -in .travis/muffin.enc -out .travis/muffin -d
 chmod 600 .travis/muffin
-echo -e "Host $DEPLOY_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host $deploy_host\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval "$(ssh-agent -s)" # start the ssh agent
 ssh-add .travis/muffin
 git remote add deploy $deploy_user@$deploy_host:$deploy_uri
