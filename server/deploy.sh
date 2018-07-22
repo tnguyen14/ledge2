4d88627ce909306e9503f9844926f0c4b616a2d9
Tri Nguyen <tri@tridnguyen.com>
2018-07-22 00:57:43 -0400

    run docker-compose on travis

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,5 +4,8 @@ chmod 600 .travis/muffin
 echo -e "Host $deploy_host\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 eval "$(ssh-agent -s)" # start the ssh agent
 ssh-add .travis/muffin
+
 git remote add deploy $deploy_user@$deploy_host:$deploy_uri
 git push deploy master
+
+ssh $deploy_user@@$deploy_host "cd $deploy_uri; docker-compose up --build -d"
