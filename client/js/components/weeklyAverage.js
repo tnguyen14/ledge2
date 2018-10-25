51c783890129befee3d075d08ef0b14195d1cc8b
Tri Nguyen <tri@tridnguyen.com>
2018-10-25 01:35:28 -0400

    update weekly average label

--- client/js/components/weeklyAverage.js
+++ client/js/components/weeklyAverage.js
@@ -4,10 +4,21 @@ import money from '../util/money';
 
 function WeeklyAverage(props) {
 	const { numWeeks, weeks, weeklyAverage } = props;
-	const numMonths = numWeeks / 4;
-	const label = `Last ${numWeeks} weeks (~${numMonths} ${
-		numMonths === 1 ? 'month' : 'months'
-	})`;
+	let duration;
+	switch (numWeeks) {
+		case 4:
+			duration = 'month';
+			break;
+		case 12:
+			duration = 'quarter';
+			break;
+		case 24:
+			duration = '6 months';
+			break;
+		default:
+			duration = `${numWeeks} weeks`;
+	}
+	const label = `Last ${duration}`;
 	const value = weeks.length ? money(weeklyAverage) : 'Calculating...';
 	return (
 		<tr className="stat">
