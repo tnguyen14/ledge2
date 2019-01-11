a3c0b7f6b96b9da9bedf915174f0cc42ad20201c
Tri Nguyen <tri@tridnguyen.com>
2019-01-11 00:49:51 -0500

    allow inline css comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+	syntax: 'postcss-scss',
 	plugins: [
 		require('postcss-import'),
 		require('postcss-color-function'),
 		require('postcss-utilities'),
-		require('postcss-nested')
+		require('postcss-nested'),
+		require('postcss-strip-inline-comments')
 	]
 };
