1b2b3bb8498291de66458ce14a682da4044f0b2c
Tri Nguyen <tri@tridnguyen.com>
2020-04-13 00:32:00 -0400

    Fix look back

--- client/js/actions/weeks.js
+++ client/js/actions/weeks.js
@@ -3,37 +3,38 @@ import config from 'config';
 export const ADD_WEEK = 'ADD_WEEK';
 
 export function addWeek(index) {
-	return {
-		type: ADD_WEEK,
-		data: {
-			index
-		}
-	};
+  return {
+    type: ADD_WEEK,
+    data: {
+      index
+    }
+  };
 }
 
 export const SHOW_WEEK = 'SHOW_WEEK';
 
 export function showMore(ahead) {
-	return function (dispatch, getState) {
-		const { weeks } = getState();
-		// get all the visible weeks' indices, sort from high to low
-		const visibleWeeksIndices = Object.keys(weeks)
-			.filter((weekIndex) => weeks[weekIndex].visible)
-			.sort((a, b) => b - a);
-		const nextIndex = ahead
-			? Number(visibleWeeksIndices[0]) + 1
-			: Number(visibleWeeksIndices.pop()) - 1;
-		// the next week doesn't exist
-		if (!weeks[nextIndex] || !weeks[nextIndex].hasLoaded) {
-			dispatch(addWeek(nextIndex));
-		}
-		dispatch({
-			type: SHOW_WEEK,
-			data: {
-				index: nextIndex
-			}
-		});
-	};
+  return function (dispatch, getState) {
+    const { weeks } = getState();
+    // get all the visible weeks' indices, sort from high to low
+    const visibleWeeksIndices = Object.keys(weeks)
+      .filter((weekIndex) => weeks[weekIndex].visible)
+      .sort((a, b) => b - a);
+    const nextIndex =
+      ahead == true
+        ? Number(visibleWeeksIndices[0]) + 1
+        : Number(visibleWeeksIndices.pop()) - 1;
+    // the next week doesn't exist
+    if (!weeks[nextIndex] || !weeks[nextIndex].hasLoaded) {
+      dispatch(addWeek(nextIndex));
+    }
+    dispatch({
+      type: SHOW_WEEK,
+      data: {
+        index: nextIndex
+      }
+    });
+  };
 }
 
 export const LOAD_TRANSACTIONS = 'LOAD_TRANSACTIONS';
@@ -42,27 +43,27 @@ export const LOAD_TRANSACTIONS_SUCCESS = 'LOAD_TRANSACTIONS_SUCCESS';
 const serverUrl = process.env.SERVER_URL;
 
 export function loadTransactions(offset) {
-	return function (dispatch, getState) {
-		dispatch({
-			type: LOAD_TRANSACTIONS,
-			data: {
-				offset
-			}
-		});
-		getJson
-			.bind(
-				null,
-				dispatch,
-				getState
-			)(`${serverUrl}/accounts/${config.account_name}/weekly/${offset}`)
-			.then((transactions) => {
-				dispatch({
-					type: LOAD_TRANSACTIONS_SUCCESS,
-					data: {
-						offset,
-						transactions
-					}
-				});
-			});
-	};
+  return function (dispatch, getState) {
+    dispatch({
+      type: LOAD_TRANSACTIONS,
+      data: {
+        offset
+      }
+    });
+    getJson
+      .bind(
+        null,
+        dispatch,
+        getState
+      )(`${serverUrl}/accounts/${config.account_name}/weekly/${offset}`)
+      .then((transactions) => {
+        dispatch({
+          type: LOAD_TRANSACTIONS_SUCCESS,
+          data: {
+            offset,
+            transactions
+          }
+        });
+      });
+  };
 }
--- client/js/containers/weeks.js
+++ client/js/containers/weeks.js
@@ -6,39 +6,33 @@ import { showMore } from '../actions/weeks';
 import Week from './week';
 
 function Weeks(props) {
-	const { weeks, showMore } = props;
-	return (
-		<div className="transactions">
-			<Button onClick={showMore.bind(null, true)}>Look Ahead</Button>
-			{Object.keys(weeks)
-				.sort((a, b) => b - a)
-				.map((week) => {
-					return (
-						<Week
-							key={week}
-							offset={Number(week)}
-							{...weeks[week]}
-						/>
-					);
-				})}
-			<Button variant="success" onClick={showMore}>
-				Show More
-			</Button>
-		</div>
-	);
+  const { weeks, showMore } = props;
+  return (
+    <div className="transactions">
+      <Button onClick={showMore.bind(null, true)}>Look Ahead</Button>
+      {Object.keys(weeks)
+        .sort((a, b) => b - a)
+        .map((week) => {
+          return <Week key={week} offset={Number(week)} {...weeks[week]} />;
+        })}
+      <Button variant="success" onClick={showMore.bind(null, false)}>
+        Show More
+      </Button>
+    </div>
+  );
 }
 
 Weeks.propTypes = {
-	weeks: PropTypes.object.isRequired,
-	showMore: PropTypes.func.isRequired
+  weeks: PropTypes.object.isRequired,
+  showMore: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
-	return {
-		weeks: state.weeks
-	};
+  return {
+    weeks: state.weeks
+  };
 }
 
 export default connect(mapStateToProps, {
-	showMore
+  showMore
 })(Weeks);
