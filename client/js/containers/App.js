e9420bf0eb99eed05243f50d9103d840a18221da
Tri Nguyen <tri@tridnguyen.com>
2020-04-21 21:15:47 -0400

    Use hooks for App

--- client/js/containers/App.js
+++ client/js/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Form from './form';
 import AverageStats from './averageStats';
 import Weeks from './weeks';
@@ -10,66 +10,66 @@ import { connect } from 'react-redux';
 import { login, handleAuthentication } from '../actions/user';
 import history from '../history';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		const { location, handleAuthentication } = props;
-		const callbackRegex = /callback\.html/;
-		const isCallback = callbackRegex.test(location.pathname);
-		const basePath = location.pathname.replace(callbackRegex, '');
-		if (isCallback) {
-			if (location.hash) {
-				handleAuthentication();
-			}
-			history.replace(basePath);
-		}
-	}
-	render() {
-		const {
-			login,
-			authenticated,
-			isAuthenticating,
-			isRenewing
-		} = this.props;
-		const notification = isRenewing
-			? {
-					title: 'Authentication',
-					content: 'Renewing session..'
-			  }
-			: {};
-		if (isAuthenticating) {
-			return <h2 className="auth-loading">Loading...</h2>;
-		}
-		if (!authenticated) {
-			return <Login login={login} />;
-		}
-		return (
-			<div className="app">
-				<Form />
-				<AverageStats />
-				<Weeks />
-				<DeleteDialog />
-				<Notification {...notification} />
-			</div>
-		);
-	}
+function App(props) {
+  const {
+    login,
+    authenticated,
+    isAuthenticating,
+    isRenewing,
+    location,
+    handleAuthentication
+  } = props;
+
+  useEffect(() => {
+    const callbackRegex = /callback\.html/;
+    const isCallback = callbackRegex.test(location.pathname);
+    const basePath = location.pathname.replace(callbackRegex, '');
+    if (isCallback) {
+      if (location.hash) {
+        handleAuthentication();
+      }
+      history.replace(basePath);
+    }
+  }, []);
+
+  const notification = isRenewing
+    ? {
+        title: 'Authentication',
+        content: 'Renewing session..'
+      }
+    : {};
+  if (isAuthenticating) {
+    return <h2 className="auth-loading">Loading...</h2>;
+  }
+  if (!authenticated) {
+    return <Login login={login} />;
+  }
+  return (
+    <div className="app">
+      <Form />
+      <AverageStats />
+      <Weeks />
+      <DeleteDialog />
+      <Notification {...notification} />
+    </div>
+  );
 }
 
 App.propTypes = {
-	login: PropTypes.func.isRequired,
-	handleAuthentication: PropTypes.func.isRequired,
-	authenticated: PropTypes.bool.isRequired,
-	isAuthenticating: PropTypes.bool,
-	isRenewing: PropTypes.bool,
-	location: PropTypes.object
+  login: PropTypes.func.isRequired,
+  handleAuthentication: PropTypes.func.isRequired,
+  authenticated: PropTypes.bool.isRequired,
+  isAuthenticating: PropTypes.bool,
+  isRenewing: PropTypes.bool,
+  location: PropTypes.object
 };
 
 function mapStateToProps(state) {
-	return {
-		...state.user
-	};
+  return {
+    ...state.user
+  };
 }
 export default connect(mapStateToProps, {
-	login,
-	handleAuthentication
+  login,
+  handleAuthentication
 })(App);
