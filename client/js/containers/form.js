01e1ae78d0b891ad4492ea08fbc3462400153195
Tri Nguyen <tri@tridnguyen.com>
2020-05-05 21:58:45 -0400

    Convert container form to react hooks

--- client/js/containers/form.js
+++ client/js/containers/form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button';
@@ -6,121 +6,113 @@ import Field from '../components/field';
 import { submitForm, inputChange, resetForm } from '../actions/form';
 import { loadAccount } from '../actions/account';
 
-class Form extends Component {
-	componentDidMount() {
-		this.props.loadAccount();
-	}
+function Form(props) {
+  const amount = props.fields.find((field) => field.name == 'amount').value;
+  useEffect(props.loadAccount, []);
 
-	componentDidUpdate(prevProps, prevState) {
-		const currentAmount = this.props.fields.find(
-			(field) => field.name === 'amount'
-		).value;
-		const prevAmount = prevProps.fields.find(
-			(field) => field.name === 'amount'
-		).value;
-		// only focus if the amount has changed
-		// avoid focusing for cases such as when categories are loaded
-		if (this.props.focus && currentAmount !== prevAmount) {
-			this.amount.focus();
-		}
-	}
+  let amountFieldRef = useRef();
 
-	handleInputChange(fieldName) {
-		return (event) => {
-			return this.props.inputChange(fieldName, event.target.value);
-		};
-	}
+  useEffect(() => {
+    console.log(amountFieldRef);
+    console.log(amountFieldRef.current);
+    if (amountFieldRef) {
+      amountFieldRef.focus();
+    }
+  }, [amount]);
 
-	render() {
-		const {
-			fields,
-			action,
-			datalists,
-			submitForm,
-			pending,
-			resetForm,
-			fieldOptions
-		} = this.props;
-		const buttonAttrs = {
-			disabled: Boolean(pending)
-		};
-		return (
-			<form className="new-transaction" method="POST">
-				<h2>Add a new transaction</h2>
-				{fields.map((field) => {
-					if (field.attributes && field.attributes.list) {
-						field.datalist = datalists[field.attributes.list];
-					}
-					if (fieldOptions[field.name]) {
-						field.options = fieldOptions[field.name];
-					}
-					return (
-						<Field
-							inputRef={(input) => {
-								this[field.name] = input;
-							}}
-							key={field.name}
-							handleChange={this.handleInputChange(field.name)}
-							{...field}
-						/>
-					);
-				})}
-				<Button
-					variant="primary"
-					className="float-right"
-					type="submit"
-					onClick={submitForm}
-					{...buttonAttrs}
-				>
-					{action}
-				</Button>
-				<Button
-					variant="outline-secondary"
-					className="float-right"
-					onClick={resetForm}
-					{...buttonAttrs}
-				>
-					Reset
-				</Button>
-			</form>
-		);
-	}
+  const {
+    fields,
+    action,
+    datalists,
+    submitForm,
+    pending,
+    resetForm,
+    fieldOptions
+  } = props;
+  const buttonAttrs = {
+    disabled: Boolean(pending)
+  };
+
+  return (
+    <form className="new-transaction" method="POST">
+      <h2>Add a new transaction</h2>
+      {fields.map((field) => {
+        if (field.attributes && field.attributes.list) {
+          field.datalist = datalists[field.attributes.list];
+        }
+        if (fieldOptions[field.name]) {
+          field.options = fieldOptions[field.name];
+        }
+        return (
+          <Field
+            inputRef={(input) => {
+              if (field.name == 'amount') {
+                amountFieldRef = input;
+              }
+            }}
+            key={field.name}
+            handleChange={(event) => {
+              props.inputChange(field.name, event.target.value);
+            }}
+            {...field}
+          />
+        );
+      })}
+      <Button
+        variant="primary"
+        className="float-right"
+        type="submit"
+        onClick={submitForm}
+        {...buttonAttrs}
+      >
+        {action}
+      </Button>
+      <Button
+        variant="outline-secondary"
+        className="float-right"
+        onClick={resetForm}
+        {...buttonAttrs}
+      >
+        Reset
+      </Button>
+    </form>
+  );
 }
 
 Form.propTypes = {
-	fields: PropTypes.array.isRequired,
-	action: PropTypes.string.isRequired,
-	focus: PropTypes.bool,
-	pending: PropTypes.bool,
-	submitForm: PropTypes.func,
-	datalists: PropTypes.shape({
-		'merchants-list': PropTypes.array
-	}),
-	loadAccount: PropTypes.func,
-	inputChange: PropTypes.func,
-	resetForm: PropTypes.func,
-	fieldOptions: PropTypes.shape({
-		category: PropTypes.array.isRequired,
-		source: PropTypes.array.isRequired
-	})
+  fields: PropTypes.array.isRequired,
+  action: PropTypes.string.isRequired,
+  focus: PropTypes.bool,
+  pending: PropTypes.bool,
+  submitForm: PropTypes.func,
+  datalists: PropTypes.shape({
+    'merchants-list': PropTypes.array
+  }),
+  loadAccount: PropTypes.func,
+  inputChange: PropTypes.func,
+  resetForm: PropTypes.func,
+  fieldOptions: PropTypes.shape({
+    category: PropTypes.array.isRequired,
+    source: PropTypes.array.isRequired
+  })
 };
 
 function mapStateToProps(state) {
-	return {
-		...state.form,
-		datalists: {
-			'merchants-list': state.account.merchants
-		},
-		fieldOptions: {
-			category: state.account.categories,
-			source: state.account.sources
-		}
-	};
+  return {
+    ...state.form,
+    datalists: {
+      'merchants-list': state.account.merchants
+    },
+    fieldOptions: {
+      category: state.account.categories,
+      source: state.account.sources
+    }
+  };
 }
 
 export default connect(mapStateToProps, {
-	submitForm,
-	loadAccount,
-	inputChange,
-	resetForm
+  submitForm,
+  loadAccount,
+  inputChange,
+  resetForm
 })(Form);
