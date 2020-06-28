71d35003f72fba66dd9d2a57ac17cce7194f5d69
Tri Nguyen <tri@tridnguyen.com>
2020-06-28 17:03:22 -0400

    Use @tridnguyen/auth module

--- client/js/reducers/user.js
+++ client/js/reducers/user.js
@@ -1,95 +1,68 @@
 /* global localStorage */
 import {
-	AUTHENTICATING,
-	AUTHENTICATED,
-	RENEWING_SESSION,
-	RENEWED_SESSION,
-	SCHEDULE_RENEWAL,
-	LOGOUT
+  AUTHENTICATING,
+  AUTHENTICATED,
+  RENEWING_SESSION,
+  RENEWED_SESSION,
+  SCHEDULE_RENEWAL,
+  LOGOUT
 } from '../actions/user';
-import getUser from '../util/user';
+import { getSession, storeSession, deleteSession } from '@tridnguyen/auth';
 
 const initialState = {
-	authenticated: false,
-	isAuthenticating: false,
-	isRenewing: false
+  authenticated: false,
+  isAuthenticating: false,
+  isRenewing: false
 };
 
-function isAuthenticated(user) {
-	return Date.now() < user.expiresAt;
-}
-
-function storeSession(user) {
-	localStorage.setItem('access_token', user.accessToken);
-	localStorage.setItem('id_token', user.idToken);
-	localStorage.setItem('expires_at', JSON.stringify(user.expiresAt));
-}
-
-function deleteSession() {
-	localStorage.removeItem('access_token');
-	localStorage.removeItem('id_token');
-	localStorage.removeItem('expires_at');
-}
-
-function getSession() {
-	const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-	if (isAuthenticated({ expiresAt })) {
-		// Auth0 example does not retrieve these tokens
-		// but it seems like they should be
-		const accessToken = localStorage.getItem('access_token');
-		const idToken = localStorage.getItem('id_token');
-		return {
-			authenticated: true,
-			expiresAt,
-			accessToken,
-			idToken,
-			profile: getUser(idToken)
-		};
-	}
-	// if not authenticated, remove any existing session
-	deleteSession();
-}
-
 export default function user(state = initialState, action) {
-	switch (action.type) {
-		case AUTHENTICATING:
-			return {
-				...state,
-				isAuthenticating: true,
-				authenticated: false
-			};
-		case RENEWING_SESSION:
-			return {
-				...state,
-				isRenewing: true
-			};
-		case AUTHENTICATED:
-		case RENEWED_SESSION:
-			const { accessToken, idToken, expiresIn } = action.data;
-			const expiresAt = expiresIn * 1000 + Date.now();
-			const newState = {
-				...state,
-				isAuthenticating: false,
-				isRenewing: false,
-				authenticated: isAuthenticated({ expiresAt }),
-				expiresAt,
-				accessToken,
-				idToken,
-				profile: getUser(idToken)
-			};
-			delete newState.renewTimeout;
-			storeSession(newState);
-			return newState;
-		case SCHEDULE_RENEWAL:
-			return {
-				...state,
-				renewTimeout: action.data
-			};
-		case LOGOUT:
-			deleteSession();
-			clearTimeout(state.renewTimeout);
-			return initialState;
-		default:
-			return getSession() || state;
-	}
+  switch (action.type) {
+    case AUTHENTICATING:
+      return {
+        ...state,
+        isAuthenticating: true,
+        authenticated: false
+      };
+    case RENEWING_SESSION:
+      return {
+        ...state,
+        isRenewing: true
+      };
+    case AUTHENTICATED:
+    case RENEWED_SESSION:
+      const { accessToken, idToken, expiresIn } = action.data;
+      const expiresAt = expiresIn * 1000 + Date.now();
+      storeSession({
+        accessToken,
+        idToken,
+        expiresAt
+      });
+      const newSession = getSession();
+      const newState = {
+        ...state,
+        ...newSession,
+        authenticated: true,
+        isAuthenticating: false,
+        isRenewing: false
+      };
+      delete newState.renewTimeout;
+      return newState;
+    case SCHEDULE_RENEWAL:
+      return {
+        ...state,
+        renewTimeout: action.data
+      };
+    case LOGOUT:
+      deleteSession();
+      clearTimeout(state.renewTimeout);
+      return initialState;
+    default:
+      const session = getSession();
+      return session
+        ? {
+            ...session,
+            authenticated: true
+          }
+        : state;
+  }
 }
