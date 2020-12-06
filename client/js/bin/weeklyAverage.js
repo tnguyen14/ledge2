9b91a6f824e2364946291836edb496336b82b418
Tri Nguyen <tri@tridnguyen.com>
2020-12-06 16:31:59 -0500

    Script to calculate yearly averages

---
+++ client/js/bin/weeklyAverage.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const moment = require('moment-timezone');
+const { getJson } = require('simple-fetch');
+const qs = require('qs');
+const { usd } = require('@tridnguyen/money');
+
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
+const SERVER_URL = 'https://lists.cloud.tridnguyen.com/ledge/tri';
+const TIMEZONE = 'America/New_York';
+
+function sum(nums) {
+  if (!Array.isArray(nums)) {
+    console.log(`${nums} is not array`);
+    return 0;
+  }
+  return nums.reduce(function (subtotal, num) {
+    return subtotal + num;
+  }, 0);
+}
+
+async function getWeeklyAverage(start, end) {
+  const startMonday = start.isoWeekday(1).startOf('day');
+  const endMonday = end.isoWeekday(8).startOf('day');
+  const numWeeks = endMonday.diff(startMonday, 'week');
+
+  const query = qs.stringify({
+    where: [
+      {
+        field: 'date',
+        op: '>=',
+        value: startMonday.toISOString()
+      },
+      {
+        field: 'date',
+        op: '<',
+        value: endMonday.toISOString()
+      }
+    ]
+  });
+
+  const transactions = await getJson(`${SERVER_URL}/items?${query}`, {
+    headers: {
+      Authorization: `Bearer ${AUTH_TOKEN}`
+    }
+  });
+  const total = sum(transactions.map((t) => t.amount));
+  const average = total / numWeeks;
+  console.log(`Getting average for
+${startMonday.toISOString()} - ${endMonday.toISOString()} (${numWeeks} weeks)`);
+
+  return usd(average);
+}
+
+async function year(y) {
+  const now = moment().tz(TIMEZONE);
+  const start = moment(`${y}-01-01`).tz(TIMEZONE);
+  let end = moment(`${y}-12-31`).tz(TIMEZONE);
+  if (now.isBefore(end)) {
+    end = now;
+  }
+  return await getWeeklyAverage(start, end);
+}
+
+year('2019').then(console.log, console.error);
+year('2020').then(console.log, console.error);
