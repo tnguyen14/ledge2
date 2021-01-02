ccdebba7a9adc35952de1697559118e5aeb1788e
Tri Nguyen <tri@tridnguyen.com>
2021-01-01 22:30:58 -0500

    intercept network requests to wait

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,3 +1,4 @@
+const SERVER_URL = 'https://api.tridnguyen.com/lists/ledge/tri';
 describe('Open app', () => {
   beforeEach(() => {
     cy.restoreLocalStorage();
@@ -9,7 +10,18 @@ describe('Open app', () => {
     cy.saveLocalStorage();
   });
   it('Display logged in content', () => {
+    cy.intercept(`${SERVER_URL}/meta`).as('accountMeta');
+    cy.intercept(`${SERVER_URL}/items?*`).as('weeks');
+
+    // reload the page to capture network requests
+    cy.visit('/');
+
     cy.contains('Add a new transaction');
     cy.contains('Weekly Averages');
+
+    // account meta is loaded on the form
+    cy.wait('@accountMeta');
+    cy.get('select[name=category]').should('have.value', 'dineout');
+    cy.get('select[name=source]').should('have.value', 'chase-sapphire');
   });
 });
