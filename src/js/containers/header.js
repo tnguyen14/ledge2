c7e287efc563f9dba6e4d42c58dcf05bbaf2df07
Tri Nguyen <tri@tridnguyen.com>
2021-01-18 23:30:40 -0500

    Rename client to src

--- src/js/containers/header.js
+++ src/js/containers/header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import UserMenu from './userMenu';
 
 function Header() {
-	return (
-		<div className="header">
-			<h1>Ledge</h1>
-			<UserMenu />
-		</div>
-	);
+  return (
+    <div className="header">
+      <h1>Ledge</h1>
+      <UserMenu />
+    </div>
+  );
 }
 
 export default Header;
