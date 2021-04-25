97cb42f1f57141d42007b9dc7d1b87aeea9c9d71
Tri Nguyen <tri@tridnguyen.com>
2021-04-25 14:25:22 -0400

    transaction operations

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import form from './form';
 import account from './account';
-import weeks from './weeks';
 import user from './user';
 import app from './app';
 import transactions from './transactions';
@@ -10,7 +9,6 @@ const rootReducer = combineReducers({
   app,
   form,
   account,
-  weeks,
   user,
   transactions
 });
--- js/reducers/transactions.js
+++ js/reducers/transactions.js
@@ -1,5 +1,11 @@
 import { LOAD_YEARS_SUCCESS } from '../actions/years';
 import { LOAD_WEEK_SUCCESS } from '../actions/weeks';
+import {
+  ADD_TRANSACTION_SUCCESS,
+  UPDATE_TRANSACTION_SUCCESS,
+  REMOVE_TRANSACTION_SUCCESS
+} from '../actions/transaction';
+
 export default function transactions(state = {}, action) {
   switch (action.type) {
     case LOAD_YEARS_SUCCESS:
@@ -13,6 +19,20 @@ export default function transactions(state = {}, action) {
         },
         { ...state }
       );
+    case ADD_TRANSACTION_SUCCESS:
+      return {
+        ...state,
+        [action.data.id]: action.data
+      };
+    case REMOVE_TRANSACTION_SUCCESS:
+      const newState = { ...state };
+      delete newState[action.data];
+      return newState;
+    case UPDATE_TRANSACTION_SUCCESS:
+      return {
+        ...state,
+        [action.data.id]: action.data
+      };
     default:
       return state;
   }
