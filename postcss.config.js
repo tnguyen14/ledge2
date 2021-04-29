ff8372f20703088cabdbb5d8c5991c8c52d1be28
Tri Nguyen <tri@tridnguyen.com>
2021-04-28 23:25:39 -0400

    Change scss to css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-	syntax: 'postcss-scss',
-	plugins: [
-		require('postcss-import'),
-		require('postcss-color-function'),
-		require('postcss-utilities'),
-		require('postcss-nested'),
-		require('postcss-strip-inline-comments')
-	]
+  plugins: [require('postcss-import'), require('postcss-nested')]
 };
