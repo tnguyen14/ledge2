97fa259113284345ede53122e53dc6bade8303ac
Tri Nguyen <tri@tridnguyen.com>
2021-05-09 11:14:45 -0400

    Remove useMediaQuery from Field

--- js/components/field.js
+++ js/components/field.js
@@ -2,9 +2,6 @@ import React from 'https://cdn.skypack.dev/react@16';
 import { InfoIcon } from 'https://cdn.skypack.dev/@primer/octicons-react@11';
 import OverlayTrigger from 'https://cdn.skypack.dev/react-bootstrap@1/OverlayTrigger';
 import Tooltip from 'https://cdn.skypack.dev/react-bootstrap@1/Tooltip';
-import classnames from 'https://cdn.skypack.dev/classnames@2';
-import reactResponsive from 'https://cdn.skypack.dev/react-responsive@8';
-const { useMediaQuery } = reactResponsive;
 
 const inputTypes = ['text', 'date', 'time', 'number', 'hidden'];
 function Field(props) {
@@ -24,9 +21,7 @@ function Field(props) {
     afterButtonAction,
     tabindex = 0
   } = props;
-  const isSmallScreen = useMediaQuery({
-    query: '(max-width: 42.5em)'
-  });
+
   let inputEl;
   if (inputTypes.includes(type)) {
     inputEl = (
@@ -81,13 +76,7 @@ function Field(props) {
   }
   return (
     <div className="form-group" data-form-name={name}>
-      <label
-        className={classnames('control-label', {
-          'sr-only': isSmallScreen
-        })}
-      >
-        {label}
-      </label>
+      <label className="control-label">{label}</label>
       <div className="input-group">
         {hint && (
           <OverlayTrigger
