2ddee16eaafd17ae552e97f448379b9724785b77
Tri Nguyen <tri@tridnguyen.com>
2021-05-09 13:31:30 -0400

    Use details element for carried overs

--- js/components/weeks/weekCategory.js
+++ js/components/weeks/weekCategory.js
@@ -1,7 +1,5 @@
 import React from 'https://cdn.skypack.dev/react@16';
 import { usd } from 'https://cdn.skypack.dev/@tridnguyen/money@1';
-import Popover from 'https://cdn.skypack.dev/react-bootstrap@1/Popover';
-import OverlayTrigger from 'https://cdn.skypack.dev/react-bootstrap@1/OverlayTrigger';
 import { format } from 'https://cdn.skypack.dev/date-fns@2';
 import { utcToZonedTime } from 'https://cdn.skypack.dev/date-fns-tz@1';
 import { ClockIcon } from 'https://cdn.skypack.dev/@primer/octicons-react@11';
@@ -10,41 +8,30 @@ import { TIMEZONE } from '../../util/constants.js';
 function WeekCategory(props) {
   const { slug, label, amount, weekId, carriedOvers = [] } = props;
   const statId = `${slug}-${weekId}`;
-  const popover = (
-    <Popover
-      id={`carried-${statId}`}
-      className="carriedover-transactions"
-      title="Carried-over transactions"
-    >
-      {carriedOvers.map((txn) => {
-        const dateInZone = utcToZonedTime(txn.date, TIMEZONE);
-        return (
-          <li key={txn.id}>
-            {format(dateInZone, 'MM/dd/yy')} ({txn.span}) {txn.merchant}{' '}
-            {usd(txn.amount)}{' '}
-          </li>
-        );
-      })}
-    </Popover>
-  );
   return (
-    <tr key={statId} className="stat" data-cat={slug}>
-      {carriedOvers.length ? (
-        <OverlayTrigger trigger="click" overlay={popover}>
-          <td id={statId} className={`stat-label carried-over`}>
-            <span className="legend">&nbsp;</span>
-            {label}
-            <span className="span-hint">
+    <tr key={statId} id={statId} className="stat" data-cat={slug}>
+      <td className="stat-label">
+        <span className="legend">&nbsp;</span>
+        {label}
+        {carriedOvers.length && (
+          <details className="span-hint">
+            <summary>
               <ClockIcon />
-            </span>
-          </td>
-        </OverlayTrigger>
-      ) : (
-        <td id={statId} className="stat-label">
-          <span className="legend">&nbsp;</span>
-          {label}
-        </td>
-      )}
+            </summary>
+            <ul>
+              {carriedOvers.map((txn) => {
+                const dateInZone = utcToZonedTime(txn.date, TIMEZONE);
+                return (
+                  <li key={txn.id}>
+                    {format(dateInZone, 'MM/dd/yy')} ({txn.span}) {txn.merchant}{' '}
+                    {usd(txn.amount)}{' '}
+                  </li>
+                );
+              })}
+            </ul>
+          </details>
+        )}
+      </td>
       <td aria-labelledby={statId}>{usd(amount)}</td>
     </tr>
   );
