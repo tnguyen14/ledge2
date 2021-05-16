fc6371d05d22e4f5f27d6a7c793a520ef9fe32d6
Tri Nguyen <tri@tridnguyen.com>
2021-05-16 13:43:57 -0400

    Hack to load skypack assets

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,9 @@
  * @format
  */
 
+const Resolver = require('metro-resolver');
+const path = require('path');
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -14,4 +17,17 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    // simplistic hack to allow resolving skypack assets
+    // with node_modules in native
+    resolveRequest: (context, realModuleName, platform, moduleName) => {
+      let name = moduleName;
+      if (moduleName.startsWith('https://')) {
+        name = moduleName.replace(/https:\/\/cdn\.skypack\.dev\//, '').replace(/@\d+/, '')
+        console.log(`Replacing ${moduleName} with ${name}`);
+      }
+      return Resolver.resolve({...context, resolveRequest: undefined}, name, platform)
+    },
+    nodeModulesPaths: [path.resolve('native/node_modules')]
+  }
 };
