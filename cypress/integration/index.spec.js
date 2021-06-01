6cc8527a6dd3658ab185d453e3022989b6452fcb
Tri Nguyen <tri@tridnguyen.com>
2021-06-01 01:16:07 -0400

    do before in index

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,8 +2,20 @@ import { display } from './display.spec.js';
 import { add } from './add.spec.js';
 import { amount, merchant } from './update.spec.js';
 import { remove } from './delete.spec.js';
+import { loginButton } from '../selectors';
 
 describe('Ledge', () => {
+  before(() => {
+    cy.visit('/');
+    cy.get('body').then(($body) => {
+      if ($body.find(loginButton).length) {
+        cy.log('Login required - performing login');
+        cy.login();
+        cy.saveLocalStorage();
+        cy.reload();
+      }
+    });
+  });
   it('Display logged in content', display);
   it('Add a transaction', add);
   it('Update transaction amount', amount);
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import 'cypress-failed-log';
-import './before.js';
 import './beforeEach.js';
 
 // Alternatively you can use CommonJS syntax:
