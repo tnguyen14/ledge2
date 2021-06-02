d9a36d3c08a1b71118dec03fa6a0e77e83872225
Tri Nguyen <tri@tridnguyen.com>
2021-06-01 23:11:58 -0400

    get access token

--- native/App.js
+++ native/App.js
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   Button,
@@ -18,12 +18,46 @@ import {
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
+import Auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({
+  domain: 'tridnguyen.auth0.com',
+  clientId: 'z3IK464A6PogdpKe0LY0vTaKr6izei2a'
+});
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
   };
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [accessToken, setAccessToken] = useState();
+  const [error, setError] = useState();
+
+  async function login() {
+    try {
+      const creds = await auth0.webAuth.authorize({
+        scope: 'openid profile email',
+        audience: 'https://lists.cloud.tridnguyen.com'
+      });
+      setAccessToken(creds.accessToken);
+      setLoggedIn(true);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function logout() {
+    try {
+      await auth0.webAuth.clearSession({});
+      setAccessToken(null);
+      setLoggedIn(false);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -33,7 +67,8 @@ const App = () => {
           backgroundColor: isDarkMode ? Colors.black : Colors.white
         }}
       >
-        <Button title="Log In" />
+        {loggedIn && <Text>Access token: ${accessToken}</Text>}
+        {!loggedIn && <Button title="Log In" onPress={login} />}
       </View>
     </SafeAreaView>
   );
