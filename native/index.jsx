9e610ce9505478f0cb8b8928e11cea2a89f58c5f
Tri Nguyen <tri@tridnguyen.com>
2021-06-02 23:07:43 -0400

    Use react-router-native

--- native/index.jsx
+++ native/index.jsx
@@ -1,13 +1,10 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import React from 'react';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
+import { NativeRouter, Route } from 'react-router-native';
 import App from './components/App/index.jsx';
 import reducer from '../src/reducers/index.js';
 import { name as appName } from '../static/manifest.json';
@@ -16,6 +13,10 @@ const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 AppRegistry.registerComponent(appName, () => () => (
   <Provider store={store}>
-    <App />
+    <NativeRouter>
+      <View>
+        <Route path="/" component={App} />
+      </View>
+    </NativeRouter>
   </Provider>
 ));
