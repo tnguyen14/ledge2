ef3ac83c4b917bdcf6b4ff413988feeb88c54c8d
Tri Nguyen <tri@tridnguyen.com>
2021-06-06 18:28:19 -0400

    update test

--- src/selectors/week.test.js
+++ src/selectors/week.test.js
@@ -1,18 +1,26 @@
 import { getWeekStart, getWeekEnd, getWeekId } from './week.js';
+import { expect } from 'https://cdn.skypack.dev/chai';
 
-test('weekStart - Sunday night NY after midnight UTC', () => {
-  const state = {
-    date: '2021-05-03T03:04:00.000Z'
-  };
+describe('selectors/week', () => {
+  it('weekstart', () => {});
+  it('weekstart - Sunday night NY after midnight UTC', () => {
+    const state = {
+      date: '2021-05-03T03:04:00.000Z'
+    };
 
-  expect(getWeekStart(state).toISOString()).toBe('2021-04-26T04:00:00.000Z');
-  expect(getWeekEnd(state).toISOString()).toBe('2021-05-03T03:59:59.999Z');
-  expect(getWeekId(state)).toBe('2021-04-26');
+    expect(getWeekStart(state).toISOString()).to.equal(
+      '2021-04-26T04:00:00.000Z'
+    );
+    expect(getWeekEnd(state).toISOString()).to.equal(
+      '2021-05-03T03:59:59.999Z'
+    );
+    expect(getWeekId(state)).to.equal('2021-04-26');
 
-  // Date object
-  expect(
-    getWeekStart({
-      date: new Date('2021-05-03T03:04:00.000Z')
-    }).toISOString()
-  ).toBe('2021-04-26T04:00:00.000Z');
+    // Date object
+    expect(
+      getWeekStart({
+        date: new Date('2021-05-03T03:04:00.000Z')
+      }).toISOString()
+    ).to.equal('2021-04-26T04:00:00.000Z');
+  });
 });
