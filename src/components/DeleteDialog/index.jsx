b350e47b1edb1c0c4f54e305cb3cc74623b6c499
Tri Nguyen <tri@tridnguyen.com>
2021-07-07 10:15:39 -0400

    Pin version for breakage

--- src/components/DeleteDialog/index.jsx
+++ src/components/DeleteDialog/index.jsx
@@ -3,11 +3,11 @@ import {
   useDispatch,
   useSelector
 } from 'https://cdn.skypack.dev/react-redux@7';
-import Dialog from 'https://cdn.skypack.dev/@material-ui/core@4/Dialog';
-import DialogTitle from 'https://cdn.skypack.dev/@material-ui/core@4/DialogTitle';
-import DialogContent from 'https://cdn.skypack.dev/@material-ui/core@4/DialogContent';
-import DialogContentText from 'https://cdn.skypack.dev/@material-ui/core@4/DialogContentText';
-import DialogActions from 'https://cdn.skypack.dev/@material-ui/core@4/DialogActions';
+import Dialog from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/Dialog';
+import DialogTitle from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/DialogTitle';
+import DialogContent from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/DialogContent';
+import DialogContentText from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/DialogContentText';
+import DialogActions from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/DialogActions';
 import Button from 'https://cdn.skypack.dev/react-bootstrap@1/Button';
 import { CANCEL_REMOVE_TRANSACTION } from '../../actions/account.js';
 import { removeTransaction } from '../../actions/transaction.js';
