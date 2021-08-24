d008da73e2d35fba67e8698c55c74a4a842b6136
Tri Nguyen <tri@tridnguyen.com>
2021-08-24 00:50:11 -0400

    Simplify weekly averages

--- src/components/AccountStats/WeeklyAverages.jsx
+++ src/components/AccountStats/WeeklyAverages.jsx
@@ -3,6 +3,8 @@ import { useSelector } from 'https://cdn.skypack.dev/react-redux@7';
 import { usd } from 'https://cdn.skypack.dev/@tridnguyen/money@1';
 import { format } from 'https://cdn.skypack.dev/date-fns@2';
 import { utcToZonedTime } from 'https://cdn.skypack.dev/date-fns-tz@1';
+import Tooltip from 'https://cdn.skypack.dev/@material-ui/core@4/Tooltip';
+
 import { average, weeklyTotal } from '../../util/calculate.js';
 import { getWeekStart, getWeekEnd, getWeekId } from '../../selectors/week.js';
 import { getWeekById } from '../../selectors/transactions.js';
@@ -11,10 +13,6 @@ import { TIMEZONE } from '../../util/constants.js';
 function WeeklyAverages(props) {
   const transactions = useSelector((state) => state.transactions);
   const timespans = [
-    {
-      start: 0,
-      end: -4
-    },
     {
       start: -1,
       end: -5
@@ -48,9 +46,17 @@ function WeeklyAverages(props) {
           {timespans.map((span, index) => (
             <tr className="stat" key={index}>
               <td>
-                {format(utcToZonedTime(span.startWeekEnd, TIMEZONE), 'MMM d')} -{' '}
-                {format(utcToZonedTime(span.endWeekStart, TIMEZONE), 'MMM d')} (
-                {span.start - span.end} weeks)
+                <Tooltip
+                  title={`${format(
+                    utcToZonedTime(span.startWeekEnd, TIMEZONE),
+                    'MMM d'
+                  )} - ${format(
+                    utcToZonedTime(span.endWeekStart, TIMEZONE),
+                    'MMM d'
+                  )}`}
+                >
+                  <span>Last {span.start - span.end} weeks</span>
+                </Tooltip>
               </td>
               <td>{usd(average(span.weeks.map(weeklyTotal)))}</td>
             </tr>
