eea175a4f2df263f583b91d39646487d8f53ebf3
Tri Nguyen <tri@tridnguyen.com>
2021-08-26 00:01:02 -0400

    Use react-table to render table

--- src/components/Cashflow/index.jsx
+++ src/components/Cashflow/index.jsx
@@ -1,6 +1,8 @@
-import React from 'https://cdn.skypack.dev/react@17';
+import React, { useMemo } from 'https://cdn.skypack.dev/react@17';
 import { useSelector } from 'https://cdn.skypack.dev/react-redux@7';
+import Table from 'https://cdn.skypack.dev/react-bootstrap@1/Table';
 import { getPastMonthsIds } from '../../selectors/month.js';
+import { useTable } from 'https://cdn.skypack.dev/react-table@7';
 
 function Cashflow() {
   const displayFrom = useSelector((state) => state.app.displayFrom);
@@ -9,26 +11,78 @@ function Cashflow() {
     date: displayFrom,
     numMonths: 24
   }).reverse();
+  const columns = useMemo(
+    () =>
+      [
+        {
+          accessor: 'type'
+        }
+      ].concat(
+        monthsIds.map((monthId) => ({
+          Header: monthId,
+          accessor: monthId
+        }))
+      ),
+    [monthsIds]
+  );
+  const data = useMemo(
+    () => [
+      {
+        type: 'Regular Income'
+      },
+      {
+        type: 'Passive Income'
+      },
+      {
+        type: 'Transfer In'
+      },
+      {
+        type: 'Regular Expense'
+      },
+      {
+        type: 'Passive Investment'
+      },
+      {
+        type: 'Transfer Out'
+      }
+    ],
+    [monthsIds]
+  );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
   return (
     <div className="cashflow">
-      <table className="table">
+      <Table responsive {...getTableProps()}>
         <thead>
-          <tr>
-            <th></th>
-            {monthsIds.map((monthId) => (
-              <th key={monthId}>{monthId}</th>
-            ))}
-          </tr>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            {monthsIds.map((monthId) => (
-              <td key={monthId}></td>
-            ))}
-          </tr>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 }
