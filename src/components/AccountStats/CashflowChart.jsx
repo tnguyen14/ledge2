eb751e52bd5477e93ae32f350e18d9d63c9ce0ac
Tri Nguyen <tri@tridnguyen.com>
2021-08-28 16:35:36 -0400

    CashflowGraph component

--- src/components/AccountStats/CashflowChart.jsx
+++ src/components/AccountStats/CashflowChart.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'https://cdn.skypack.dev/react@17';
+import React from 'https://cdn.skypack.dev/react@17';
 import { useSelector } from 'https://cdn.skypack.dev/react-redux@7';
 import { format } from 'https://cdn.skypack.dev/date-fns@2';
+import { utcToZonedTime } from 'https://cdn.skypack.dev/date-fns-tz@1';
+import { TIMEZONE } from '../../util/constants.js';
 import { getPastMonthsIds } from '../../selectors/month.js';
 import { getMonthsCashflow } from '../../selectors/stats.js';
 import { getMonths } from '../../selectors/transactions.js';
 import Chart from '../Chart/index.js';
+import CashflowGraph from './CashflowGraph.js';
 
 function CashflowChart() {
   const displayFrom = useSelector((state) => state.app.displayFrom);
@@ -27,11 +30,21 @@ function CashflowChart() {
       <Chart
         maxHeight={18000}
         interval={3000}
+        chartBody={
+          <div className="months-graph">
+            {monthsIds.map((id) => (
+              <CashflowGraph monthId={id} data={monthsCashflow[id]} />
+            ))}
+          </div>
+        }
         xLabels={
           <div className="months-labels">
             {monthsIds.map((id) => (
               <div class="month-label">
-                {format(new Date(`${id}-01`), 'M/ yy')}
+                {format(
+                  utcToZonedTime(new Date(`${id}-01`), TIMEZONE),
+                  'M/ yy'
+                )}
               </div>
             ))}
           </div>
---
+++ src/components/AccountStats/CashflowGraph.jsx
@@ -0,0 +1,8 @@
+import React from 'https://cdn.skypack.dev/react@17';
+
+function CashflowGraph(props) {
+  const { monthId, data } = props;
+  return <div className="cashflow-graph"></div>;
+}
+
+export default CashflowGraph;
