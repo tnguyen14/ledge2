46add03c677c2d162c3c3e9df3064c1afd255f07
Tri Nguyen <tri@tridnguyen.com>
2021-09-08 23:49:54 -0400

    Don't show display from if not logged in

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,24 +3,28 @@ import {
   useSelector,
   useDispatch
 } from 'https://cdn.skypack.dev/react-redux@7';
+import { useAuth0 } from 'https://cdn.skypack.dev/@auth0/auth0-react@1';
 import { setDisplayFrom } from '../../actions/app.js';
 import UserMenu from './UserMenu.js';
 import Field from '../Form/Field.js';
 
 function Header() {
   const dispatch = useDispatch();
+  const { isAuthenticated } = useAuth0();
   const displayFrom = useSelector((state) => state.app.displayFrom);
   return (
     <div className="header">
       <h1>Ledge</h1>
-      <Field
-        className="display-from"
-        type="date"
-        value={displayFrom}
-        handleChange={(event) => {
-          dispatch(setDisplayFrom(event.target.value));
-        }}
-      />
+      {isAuthenticated && (
+        <Field
+          className="display-from"
+          type="date"
+          value={displayFrom}
+          handleChange={(event) => {
+            dispatch(setDisplayFrom(event.target.value));
+          }}
+        />
+      )}
       <UserMenu />
     </div>
   );
