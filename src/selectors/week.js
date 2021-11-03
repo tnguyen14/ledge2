a5ffe8653ae6a2f4b997caf9ee5fc97340d45775
Tri Nguyen <tri@tridnguyen.com>
2021-11-03 13:22:26 -0400

    ISO

--- src/selectors/week.js
+++ src/selectors/week.js
@@ -1,5 +1,9 @@
 import { createSelector } from 'https://cdn.skypack.dev/reselect@4';
-import { setISODay, format } from 'https://cdn.skypack.dev/date-fns@2';
+import {
+  setISODay,
+  format,
+  parseISO
+} from 'https://cdn.skypack.dev/date-fns@2';
 import { DateTime } from 'https://cdn.skypack.dev/luxon@2';
 import { TIMEZONE, DATE_FIELD_FORMAT } from '../util/constants.js';
 
@@ -13,7 +17,7 @@ export const getDate = (state) => {
     dateStr = state.date.toISOString();
   }
   return format(
-    DateTime.fromISO(dateStr, { zone: 'UTC' }).setZone(TIMEZONE).toJSDate(),
+    parseISO(DateTime.fromISO(dateStr).setZone(TIMEZONE).toISO()),
     DATE_FIELD_FORMAT
   );
 };
