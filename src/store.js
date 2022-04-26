e7ecb3b945799f5cbc118e8079f05f965f6d920a
Tri Nguyen <tri@tridnguyen.com>
2022-04-25 23:36:42 -0400

    use configureStore

--- src/store.js
+++ src/store.js
@@ -1,8 +1,6 @@
-import { createStore, applyMiddleware } from 'https://cdn.skypack.dev/redux@3';
-import thunk from 'https://cdn.skypack.dev/redux-thunk@2';
-import { composeWithDevTools } from 'https://cdn.skypack.dev/redux-devtools-extension@2';
+import { configureStore } from 'https://cdn.skypack.dev/@reduxjs/toolkit';
 import reducer from './reducers/index.js';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = configureStore({ reducer });
 
 export default store;
