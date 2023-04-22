e7119f8ea0c4a989d7dc3d6cc1a2d9cf1a0278d9
Tri Nguyen <tri@tridnguyen.com>
2023-04-22 13:18:03 +0000

    remove native stuff

--- native/index.style.js
+++
@@ -1,9 +0,0 @@
-// import { Dimensions } from 'react-native';
-
-// const { height, width } = Dimensions.get('window');
-
-export default {
-  $rem: 16,
-  $colorGreen: '#4caf50',
-  $colorGray100: '#f5f5f5'
-};
