e0bc76e4d9ccdd5719e64cff4c83c3bcc366846c
Tri Nguyen <tri@tridnguyen.com>
2023-04-23 15:40:34 +0000

    Use pagination to select version

--- src/components/GlobalStats/Budget.jsx
+++ src/components/GlobalStats/Budget.jsx
@@ -34,31 +34,22 @@ function Budget() {
    */
   const [budget, setBudget] = useState({});
   const [versions, setVersions] = useState([]);
+  const [selectedVersion, setSelectedVersion] = useState();
 
   useEffect(() => {
     (async () => {
       try {
         setIsLoading(true);
         const commits = await octokit.rest.repos.listCommits(repo);
-        setVersions(
-          commits.data.map((commit) => ({
-            sha: commit.sha,
-            message: commit.commit.message,
-            // DO I NEED TO WORRY ABOUT TIMEZONE HERE??
-            date: new Date(commit.commit.committer.date)
-          }))
-        );
-        const weekly = await octokit.rest.repos.getContent({
-          ...repo,
-          path: 'Weekly.toml'
-        });
-        const content = atob(weekly.data.content);
-        console.log(content);
-        setBudget(
-          toml.parse(content, {
-            bigint: false
-          })
-        );
+        const budgetVersions = commits.data.map((commit) => ({
+          sha: commit.sha,
+          message: commit.commit.message,
+          // DO I NEED TO WORRY ABOUT TIMEZONE HERE??
+          date: new Date(commit.commit.committer.date)
+        }));
+        // set latest version by default
+        setSelectedVersion(budgetVersions[0].sha);
+        setVersions(budgetVersions.reverse());
         setIsLoading(false);
       } catch (e) {
         setError(e);
@@ -66,54 +57,97 @@ function Budget() {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!selectedVersion) {
+        return;
+      }
+      setError();
+      setBudget({});
+      let content;
+      try {
+        setIsLoading(true);
+        const weekly = await octokit.rest.repos.getContent({
+          ...repo,
+          path: 'Weekly.toml',
+          ref: selectedVersion
+        });
+        content = atob(weekly.data.content);
+        setIsLoading(false);
+      } catch (e) {
+        setError(e);
+        setIsLoading(false);
+      }
+      try {
+        setBudget(
+          toml.parse(content, {
+            bigint: false
+          })
+        );
+      } catch (e) {
+        console.log(content);
+        setError(e);
+      }
+    })();
+  }, [selectedVersion]);
   return (
     <div>
       <div className="version-selector">
-        {versions.length && (
-          <Pagination>
+        {versions.length ? (
+          <Pagination size="sm">
             <Pagination.Prev />
             {versions.map((version) => (
-              <Pagination.Item key={version.sha}>
+              <Pagination.Item
+                key={version.sha}
+                active={version.sha == selectedVersion}
+                data-sha={version.sha}
+                onClick={(e) => {
+                  setSelectedVersion(e.target.dataset.sha);
+                }}
+              >
                 {format(version.date, DISPLAY_DATE_FORMAT)}
               </Pagination.Item>
             ))}
             <Pagination.Next />
           </Pagination>
-        )}
+        ) : null}
       </div>
       {isLoading && <Spinner animation="border" />}
       {error && error.message}
       <table className="table table-borderless">
         <tbody>
-          {Object.entries(budget).map(([category, details]) => {
-            return (
-              <tr className="stat" key={category}>
-                <td>
-                  <details>
-                    <summary>{category}</summary>
-                    <table className="table table-borderless">
-                      <tbody>
-                        {Object.entries(details).map(
-                          ([subCategory, subDetails]) => {
-                            if (subCategory == 'amount') {
-                              return null;
-                            }
-                            return (
-                              <tr key={subCategory}>
-                                <td>{subCategory}</td>
-                                <td>{usd(subDetails.amount * 100)}</td>
-                              </tr>
-                            );
-                          }
-                        )}
-                      </tbody>
-                    </table>
-                  </details>
-                </td>
-                <td>{usd(details.amount * 100)}</td>
-              </tr>
-            );
-          })}
+          {budget &&
+            Object.entries(budget).map(([category, details]) => {
+              return (
+                <tr className="stat" key={category}>
+                  <td>
+                    <details>
+                      <summary>{category}</summary>
+                      <table className="table table-borderless">
+                        <tbody>
+                          {details &&
+                            Object.entries(details).map(
+                              ([subCategory, subDetails]) => {
+                                if (subCategory == 'amount') {
+                                  return null;
+                                }
+                                return (
+                                  <tr key={subCategory}>
+                                    <td>{subCategory}</td>
+                                    <td>{usd(subDetails.amount * 100)}</td>
+                                  </tr>
+                                );
+                              }
+                            )}
+                        </tbody>
+                      </table>
+                    </details>
+                  </td>
+                  <td>{usd(details.amount * 100)}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
