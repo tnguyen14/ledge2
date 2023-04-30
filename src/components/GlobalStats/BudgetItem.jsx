715dcde897348211910ada007d7a5b95f22411e2
Tri Nguyen <tri@tridnguyen.com>
2023-04-30 15:22:00 +0000

    tally up sub budget

--- src/components/GlobalStats/BudgetItem.jsx
+++ src/components/GlobalStats/BudgetItem.jsx
@@ -3,6 +3,7 @@ import { usd } from 'https://cdn.skypack.dev/@tridnguyen/money@1';
 import { NoteIcon } from 'https://cdn.skypack.dev/@primer/octicons-react@15';
 import Popover from 'https://cdn.skypack.dev/@material-ui/core@4.12.0/Popover';
 import PopupState from 'https://cdn.skypack.dev/material-ui-popup-state@1/hooks';
+import classnames from 'https://cdn.skypack.dev/classnames@2';
 
 const { usePopupState, bindPopover, bindTrigger } = PopupState;
 
@@ -37,6 +38,22 @@ function BudgetItem({ category, details }) {
     popupId: `${category}-memo`
   });
 
+  let subBudgetTally = 0;
+
+  const subBudget = Object.entries(details).reduce(
+    (budget, [subCategory, subDetails]) => {
+      if (subCategory == 'amount' || subCategory == 'memo') {
+        return budget;
+      }
+      budget[subCategory] = subDetails;
+      subBudgetTally += subDetails.amount;
+      return budget;
+    },
+    {}
+  );
+
+  const matchSubBudgetTally = details.amount == subBudgetTally;
+
   return (
     <>
       <tr className="stat">
@@ -56,10 +73,7 @@ function BudgetItem({ category, details }) {
             <table className="table table-borderless">
               <tbody>
                 {details &&
-                  Object.entries(details).map(([subCategory, subDetails]) => {
-                    if (subCategory == 'amount' || subCategory == 'memo') {
-                      return null;
-                    }
+                  Object.entries(subBudget).map(([subCategory, subDetails]) => {
                     return (
                       <BudgetSubCategoryItem
                         key={subCategory}
@@ -72,7 +86,9 @@ function BudgetItem({ category, details }) {
             </table>
           </details>
         </td>
-        <td>{usd(details.amount * 100)}</td>
+        <td className={classnames({ matchSubBudgetTally })}>
+          {usd(details.amount * 100)}
+        </td>
       </tr>
       <Popover {...bindPopover(memoPopupState)}>
         {details.memo && <div className="text-popover">{details.memo}</div>}
