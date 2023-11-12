2406b34e9ca20c6c651dc35785282cc52575a84d
Tri Nguyen <tri@tridnguyen.com>
2023-11-12 05:33:11 +0000

    Move reducers to store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
 import { configureStore } from 'https://esm.sh/@reduxjs/toolkit';
-import reducer from './reducers/index.js';
+import { combineReducers } from 'https://esm.sh/redux@3';
+import form from './slices/form.js';
+import meta from './slices/meta.js';
+import app from './slices/app.js';
+import transactions from './slices/transactions.js';
 
-const store = configureStore({ reducer });
+const store = configureStore({
+  reducer: combineReducers({
+    app,
+    form,
+    meta,
+    transactions
+  })
+});
 
 export default store;
