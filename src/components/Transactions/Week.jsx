872402aeeb2d1ab90587206112f2b8f794e5bece
Tri Nguyen <tri@tridnguyen.com>
2024-01-01 15:51:20 +0000

    fix week transactions display

--- src/components/Transactions/Week.jsx
+++ src/components/Transactions/Week.jsx
@@ -56,13 +56,21 @@ function Week(props) {
       <h3 className="week-title">
         {start.toFormat('LLL d')} - {end.toFormat('LLL d, yyyy')}
       </h3>
-      <table className="weekly-transactions transactions-list table table-striped">
-        <tbody>
-          {displayTransactions.map((tx) => (
-            <Transaction key={tx.id} transaction={tx} />
-          ))}
-        </tbody>
-      </table>
+      {/*
+        add a extra wrapping div over the table
+        so the table height does't expand to fill the height of the grid item
+        in the scenario where there are too few transactions on the left,
+        while the weekstats container is longer
+      */}
+      <div className="transactions-list">
+        <table className="weekly-transactions table table-striped">
+          <tbody>
+            {displayTransactions.map((tx) => (
+              <Transaction key={tx.id} transaction={tx} />
+            ))}
+          </tbody>
+        </table>
+      </div>
       <WeekStats weekId={weekId} label="Regular Expenses" />
     </div>
   );
